2d30d0a32537d0d776a26fd683ac3952d53eb8cf
Sean Cornillie <sean.cornillie@gmail.com>
2023-02-17 09:59:48 -0700

    added producer consumer dags

---
+++ dags/producer.py
@@ -0,0 +1,29 @@
+# Producer DAG controls update of our dataset #
+# Once dataset has been updated, it will trigger a separate consumer DAG to run (and consume the updates) #
+
+from airflow import DAG, Dataset
+from airflow.decorators import task
+from datetime import date, datetime
+
+my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
+
+with DAG(
+    dag_id='producer',
+    schedule='@daily',
+    start_date=datetime(2022, 1, 1),
+    catchup=False
+):
+    @task(outlets=[my_file])
+    def update_dataset():
+        with open(my_file.uri, "a+") as f:
+            f.write("producer update")
+    
+    @task(outlets=[my_file_2])
+    def update_dataset_2():
+        with open(my_file_2.uri, "a+") as f:
+            f.write("producer update")
+
+    update_dataset() >> update_dataset_2()
+
+
