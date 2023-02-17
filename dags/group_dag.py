8acc3e95de6c32a8a8747c2cb0a872aa49bd5b1b
Sean Cornillie <sean.cornillie@gmail.com>
2023-02-17 15:28:38 -0700

    added taskgroups

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,44 +1,24 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from groups.group_downloads import download_tasks
+from groups.group_transforms import transform_tasks
  
 from datetime import datetime
  
 with DAG('group_dag', start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
  
-    download_a = BashOperator(
-        task_id='download_a',
-        bash_command='sleep 10'
-    )
+    args = {'start_date': dag.start_date,
+            'schedule_interval': dag.schedule_interval,
+            'catchup': dag.catchup}
  
-    download_b = BashOperator(
-        task_id='download_b',
-        bash_command='sleep 10'
-    )
- 
-    download_c = BashOperator(
-        task_id='download_c',
-        bash_command='sleep 10'
-    )
- 
+    downloads = download_tasks()
+
     check_files = BashOperator(
         task_id='check_files',
         bash_command='sleep 10'
     )
  
-    transform_a = BashOperator(
-        task_id='transform_a',
-        bash_command='sleep 10'
-    )
+    transforms = transform_tasks()
  
-    transform_b = BashOperator(
-        task_id='transform_b',
-        bash_command='sleep 10'
-    )
- 
-    transform_c = BashOperator(
-        task_id='transform_c',
-        bash_command='sleep 10'
-    )
- 
-    [download_a, download_b, download_c] >> check_files >> [transform_a, transform_b, transform_c]
+    downloads >> check_files >> transforms
